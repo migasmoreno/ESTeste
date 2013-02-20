4f22851db275ece3a27b50d4d1f488a75190531b
migasmoreno <migas_moreno@hotmail.com>
2013-02-20 17:23:47 +0000

    2ªtentiva

--- src/Principal.java
+++ src/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1Tentativa");
+		System.out.println("2Tentativa");
 	}
 
 }
